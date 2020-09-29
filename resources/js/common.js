202e7056dba1e89f69cd2004bd1c156a0ca0e352
ThejusS <thejuspayikkattu@gmail.com>
2020-09-29 17:22:31 +0530

    Make a seperate js module file for XMLHTTPrequests and use it for menu and blogposts

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,116 +1,80 @@
+import jsonCaller from './jsonCall.js'
 window.onload = (event) => {
+  
   var obj;
   var index;
 
-  // Fetching menu objects form menu.json
-  const menuRequest = new XMLHttpRequest();
-
-  menuRequest.onload = function () {
-    if (this.status === 200) {
-      try {
-        const menuObj = JSON.parse(this.responseText);
-        // Add menu objects to html file
-        const menuList = document.querySelector("#nav-menues");
-
-        for (i = 0; i < menuObj.length; i++) {
-          menuList.appendChild(createMenu(menuObj[i].label, menuObj[i].link));
-
-          if (i == menuObj.length - 1) {
-            menuEventHandler(menuObj);
-          }
-        }
-      } catch {
-        console.warn("JSON not parsed");
-      }
-    } else {
-      console.warn("File not found");
+  
+  // Menu handler
+  const menuList = document.querySelector("#nav-menues")
+  jsonCaller('get','resources/json/menu.json',function(menuObj){
+  
+    obj = menuObj
+    for (let each_option of menuObj) {
+      let menuElements = createMenu(each_option.label, each_option.link, each_option.status)
+      menuList.appendChild(menuElements)
     }
-  };
-  menuRequest.open("get", "resources/json/menu.json");
-  menuRequest.send();
+  })
+
+  // Event handling of menu
+  menuList.addEventListener('click',function(evt){
+    let status = evt.target.getAttribute("data-status")
+   
+    if (status == "true"){
+      window.location.assign(evt.target.getAttribute("data-link"))
+    }
+    else{
+      window.location.assign("/404.html")
+    }
+  })
+
 
   // Function for creating menu elements
-  function createMenu(content, link) {
+  function createMenu(content, link, status) {
     const menuElement = document.createElement("li");
     const menuLink = document.createElement("a");
     menuLink.href = "#";
+    menuLink.setAttribute("data-link",link)
+    menuLink.setAttribute("data-status", status)
     menuLink.className += "menu-link";
     menuLink.textContent = content;
     menuElement.appendChild(menuLink);
     return menuElement;
   }
 
-  // Event handler function for menu
-  function menuEventHandler(menuObj) {
-    const menuLinks = document.querySelectorAll(".menu-link");
-    obj = menuObj;
-
-    for (i = 0; i < menuLinks.length; i++) {
-      menuLinks[i].addEventListener("click", function (event) {
-        var linktext = event.target.textContent;
-        console.log(linktext);
-        for (j = 0; j < menuLinks.length; j++) {
-          if (linktext == obj[j].label) {
-            var state = obj[j].status;
-            if (state) {
-              window.open(obj[j].link, "_self");
-            } else {
-              window.open("../404.html", "_self");
-            }
-          }
-        }
-      });
-    }
-  }
 
   // Blog post section
-  const blogRequest = new XMLHttpRequest();
+  let postLists = document.querySelector(".posts")
+  jsonCaller('get',"resources/json/blogpost.json", function(blogObj){
+     for(let each_option of blogObj){
+       let blogPost = createPost(each_option.tittle, each_option.image)
+       postLists.appendChild(blogPost)
+     }
+  })
 
-  blogRequest.onload = function () {
-    if (this.status === 200) {
-      try {
-        const blogObj = JSON.parse(this.responseText);
 
-        // Add blog post to html
-        const postLists = document.querySelector(".posts");
+  function createPost(tittle, imglink) {
+    const postElement = document.createElement("li");
+    const postLink = document.createElement("a");
 
-        function createPost(tittle, imglink) {
-          const postElement = document.createElement("li");
-          const postLink = document.createElement("a");
+    const postLinkDiv = document.createElement("div");
+    postLinkDiv.className += "post-image-div";
 
-          const postLinkDiv = document.createElement("div");
-          postLinkDiv.className += "post-image-div";
+    const postText = document.createElement("p");
+    postText.className += "post-info";
+    postText.textContent = tittle;
 
-          const postText = document.createElement("p");
-          postText.className += "post-info";
-          postText.textContent = tittle;
+    const postImg = document.createElement("img");
+    postImg.src = imglink;
 
-          const postImg = document.createElement("img");
-          postImg.src = imglink;
+    postLinkDiv.appendChild(postText);
+    postLinkDiv.appendChild(postImg);
+    postLink.appendChild(postLinkDiv);
+    postElement.appendChild(postLink);
 
-          postLinkDiv.appendChild(postText);
-          postLinkDiv.appendChild(postImg);
-          postLink.appendChild(postLinkDiv);
-          postElement.appendChild(postLink);
+    return postElement;
+  }
 
-          return postElement;
-        }
-
-        for (i = 0; i <= blogObj.length - 1; i++) {
-          postLists.appendChild(
-            createPost(blogObj[i].tittle, blogObj[i].image)
-          );
-        }
-      } catch {
-        console.warn("JSON not parsed");
-      }
-    } else {
-      console.warn("File not found");
-    }
-  };
-
-  blogRequest.open("get", "resources/json/blogpost.json");
-  blogRequest.send();
 
   //Table headings
 
