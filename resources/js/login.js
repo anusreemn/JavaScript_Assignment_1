46055dbbc6ad814553db4d46658a146e3721a8b6
ThejusS <thejuspayikkattu@gmail.com>
2020-09-30 17:33:29 +0530

    Login page won't redirect to register page if database is empty

--- resources/js/login.js
+++ resources/js/login.js
@@ -6,30 +6,12 @@ let loggedinUser = localStorage.getItem("loggedin")
 if(loggedinUser === null){
 
   if (userValue === null) {
-    let value = "[]";
+    let value = '[]';
     localStorage.setItem("users", value);
-    location.href = "/register.html";
+    loginAction()
+    // location.href = "/register.html";
   } else {
-    let fields = document.querySelectorAll("input");
-    let loginBtn = document.querySelector("#login-button");
-
-    loginBtn.addEventListener("click", function () {
-      utils.retreiveObjects("users", function (data) {
-        for (let each_option of data) {
-          if (fields[0].value == each_option.firstname) {
-            if (fields[1].value == each_option.password) {
-              let nameOfUser = `${each_option.firstname} ${each_option.lastname}`
-              localStorage.setItem("loggedin", nameOfUser)
-              location.href = "/home.html"
-            } else {
-              console.log("Wrong password")
-            }
-          } else {
-            console.log("NO user found")
-          }
-        }
-      });
-    });
+    loginAction()
   }
 
 }
@@ -37,8 +19,55 @@ else{
   location.href = "/home.html"
 }
 
+function loginAction(){
+  let fields = document.querySelectorAll("input");
+  let loginBtn = document.querySelector("#login-button");
+
+  loginBtn.addEventListener("click", function () {
+    utils.retreiveObjects("users", function (data) {
+      let dataString = JSON.stringify(data)
+      
+      if (dataString == '[]') { 
+        setError(fields[0], "No user found")
+        console.log("No user")
+      }
+      else{
+        for (let each_option of data) {
+          if (fields[0].value == each_option.firstname) {
+            clearError(fields[0])
+            if (fields[1].value == each_option.password) {
+              clearError(fields[1])
+              let nameOfUser = `${each_option.firstname} ${each_option.lastname}`
+              localStorage.setItem("loggedin", nameOfUser)
+              location.href = "/home.html"
+            } else {
+              setError(fields[1],"Wrong password")
+              console.log("No password")
+            }
+          } else {
+            setError(fields[0], "No user found")
+            console.log("No user found")
+          }
+        }
+      }
+
+    });
+  });
+}
 
 
+// Setting the error
+function setError(input, msg) {
+  const errorField = document.querySelector(`.${input.id}-error`)
+  input.style.border = "1px solid #ff0033"
+  errorField.textContent = msg
+}
 
 
+// Clearing errors
+function clearError(input) {
+  const fieldError = document.querySelector(`.${input.id}-error`)
+  input.style.border = "1px solid #2ecc71"
+  fieldError.textContent = ""
+}
 
