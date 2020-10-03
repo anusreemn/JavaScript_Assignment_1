3a4a64dcb199942aeaf86fd1b3f9c79853a5ec0d
ThejusS <thejuspayikkattu@gmail.com>
2020-10-03 16:13:02 +0530

    Checks browser compatibility for local storage api in index page

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,22 @@
-location.href = '/login.html'
+
+if(supports_local_storage()){
+  location.href = "/login.html"
+}
+else{
+  popup()
+}
+
+// Check for local storage support
+function supports_local_storage() {
+  try {
+    return "localStorage" in window && window["localStorage"] !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
+// Make the popup
+function popup(){
+  let popupCard = document.querySelector(".index-popup");
+  popupCard.classList.toggle('active')
+}
