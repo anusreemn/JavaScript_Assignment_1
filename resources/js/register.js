2f8e34da0a7bb3e891bb059fcf3c9ea3e7f02c2f
ThejusS <thejuspayikkattu@gmail.com>
2020-10-05 17:56:09 +0530

    Disable scroll while pop up appears in register page

--- resources/js/register.js
+++ resources/js/register.js
@@ -228,7 +228,18 @@ function popup(){
 
   body.classList.toggle('blur')
   popupCard.classList.toggle('active')
+  disableScroll()
 }
+
+function disableScroll() {
+  // Get the current page scroll position
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+    // if any scroll is attempted, set this to the previous value
+  window.onscroll = function () {
+    window.scrollTo(scrollLeft, scrollTop);
+  }
+} 
 
 termsError.addEventListener('change',function(e){
   if(termsError.checked){
