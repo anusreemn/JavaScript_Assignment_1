e4e13c518ab8f12eaca69b7aa7728ba86302ab19
ThejusS <thejuspayikkattu@gmail.com>
2020-10-07 13:18:52 +0530

    Created seperate table module for homepage

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,7 +1,9 @@
 import utils from './utils.js';
+import tableFn from "./table.js";
 
 var paraHeight
 let headerObj, contentObj
+
 
 /*--------------- Home page content loading section ---------------*/
 const para = document.querySelector('#blog-paragraph')
@@ -22,214 +24,55 @@ utils.jsonCaller('get','resources/json/homepage.json',function(object){
 })
 
 
-
+4
 /*--------------- Table loading section ---------------*/
+let tableHeader = document.querySelector("thead");
+let tableBody = document.querySelector('tbody')
 function loadTable(headerObj){
-  // Load table body
-  utils.jsonCaller('get', 'resources/json/vacancies.json', function (object) {
-    contentObj = object
-    tableLoader(headerObj,contentObj,'asc',null)
-  })
-}
-
-// Actual function to load table (Takes in two objects)
-function tableLoader(headerObj, contentObj, order, clickedId) {
-  const tableHeader = document.querySelector("#table-head");
-  const tableBody = document.querySelector("#table-body");
-  loadTableHead(tableHeader, headerObj, order, clickedId);
-
-  for (let contentValue of contentObj) {
-    let rowElement = document.createElement("tr");
-    for (let headerValue of headerObj) {
-      let columnElement = document.createElement("td");
-      let key = headerValue.id;
-      let cellValue = contentValue[key];
-
-      let icon = document.createElement("ion-icon");
-      icon.name = "heart";
-
-      if (headerValue.type == "link") {
-        let link = document.createElement("a");
-        link.href = cellValue;
-        link.textContent = "Link";
-        columnElement.appendChild(link);
-      } else if (headerValue.type == "button") {
-        if (contentValue.status == "open") {
-          let btn = document.createElement("button");
-          btn.textContent = "Apply Now";
-          btn.onclick = function (e) {
-            alert("Applies Successfully");
-          };
-          columnElement.appendChild(btn);
-        } else {
-          columnElement.textContent = " - ";
-        }
-      } else if (headerValue.type == "number" || headerValue.type == "date") {
-        columnElement.style.textAlign = "right";
-        columnElement.textContent = cellValue;
-        columnElement.className = key;
-      } else {
-        columnElement.textContent = cellValue;
-        columnElement.className = key;
-      }
-
-      rowElement.appendChild(columnElement);
-    }
-    tableBody.appendChild(rowElement);
-  }
-}
-
-
-// Load table headers
-function loadTableHead(tableHeader, headerObj,order,clickedId) {
-  let headRow = document.createElement("tr");
-  for (let headValue of headerObj) {
-    let headElement = document.createElement("th");
-
-    if(order == 'asc'){
-      headElement.className += 'asc'
-    }
-    else{
-      headElement.className += 'des'
-    }
-   
-    headElement.textContent = headValue.tittle;
-    headElement.setAttribute("data-type", headValue.type);
-    headElement.setAttribute("data-sortable", headValue.sortable);
-    headElement.id = headValue.id;
-    if(headValue.sortable == true){
-      let iconHolder = document.createElement("div"); // Creates a div to hold the icons
-      iconHolder.className = "icon-holder";
-      let upArrow = document.createElement("ion-icon");
-      upArrow.name = "chevron-up-outline";
-      upArrow.className = `${headValue.id}-up-arrow`; // Maps the icon and header column using class name
-      iconHolder.appendChild(upArrow);
-      let downArrow = document.createElement("ion-icon");
-      downArrow.name = "chevron-down-outline";
-      downArrow.className = `${headValue.id}-down-arrow`; // Maps the icon and header column using class name
-
-      if (clickedId != null){
-        var upIcon = document.querySelector('.JobId-up-arrow')
-        let downIcon = document.querySelector(`.${clickedId}-down-arrow`);
-        console.log(upIcon);
-        console.log(`.${clickedId}-down-arrow`);
-      }
-        // if (order == 'asc'){
-        //   upArrow.style.visibility = 'visible'
-        //   downArrow.style.visibility = 'hidden'
-        // }
-        // else{
-        //   upArrow.style.visibility = "hidden";
-        //   downArrow.style.visibility = "visible";
-        // }
-
-        iconHolder.appendChild(downArrow);
-      headElement.appendChild(iconHolder);
-    }
-    headRow.appendChild(headElement);
-
-  }
-  tableHeader.appendChild(headRow);
-}
-
-// Table sorting Area  -------------//
-const tableHeader = document.querySelector("#table-head")
-tableHeader.addEventListener('click',function(e){
   
-  let header = e.target
-  
-  if (header.dataset["sortable"] == 'true' && header.dataset['type'] == 'number'){ 
-    let element = document.querySelectorAll(`.${header.id}`)
-    let valueList = []
+  if(headerObj){  // Checks whether a tale exist
     
-    for (let each of element){
-      valueList.push(parseInt(each.textContent))
-    }
 
-    if(header.className == 'asc'){
-      console.log(header)
-      var nextOrder = 'des'
-      valueList.sort((a, b) => a - b);
-      newContentCreator(valueList, header.id, nextOrder);
-    }
-    else{
-      console.log("des");
-      var nextOrder = "asc";
-      valueList.reverse((a, b) => a - b);
-      newContentCreator(valueList, header.id, nextOrder);
-    }
-    
-  }
-  else if(header.dataset["sortable"] == 'true' && header.dataset['type'] == 'string'){
-    let element = document.querySelectorAll(`.${header.id}`)
-    let valueList = []
-    
-    for(let each of element){
-      valueList.push(each.textContent)
-    }
+    // Table header loading
+    tableFn.tableHeadLoader(headerObj, "asc", function (headRow) {
+      tableHeader.appendChild(headRow);
+    });
 
-    if (header.className == "asc") {
-      var nextOrder = "des";
-      valueList.sort();
-      newContentCreator(valueList, header.id, nextOrder);
-    } else {
-      var nextOrder = "asc";
-      valueList.reverse();
-      newContentCreator(valueList, header.id, nextOrder);
-    }
-  }
-  else if(header.dataset["sortable"] == 'true' && header.dataset['type'] == 'date'){
-    let element = document.querySelectorAll(`.${header.id}`)
-    let valueList = []
-
-    for (let each of element) {
-      valueList.push(each.textContent)
-    }
-
-    if (header.className == "asc") {
-      console.log("asc");
-      var nextOrder = "des";
-      valueList.sort();
-      newContentCreator(valueList, header.id, nextOrder);
-    } else {
-      console.log("des");
-      var nextOrder = "asc";
-      valueList.reverse();
-      newContentCreator(valueList, header.id, nextOrder);
-    }
-
-    // valueList.sort()
-    // newContentCreator(valueList, header.id);
+    // Table content loading
+    utils.jsonCaller("get", "resources/json/vacancies.json", function (object) {
+      contentObj = object;
+      tableFn.tableBodyLoader(tableBody,headerObj,contentObj)
+    });
 
   }
-  else{
-    console.log("Not sortable")
-  }
-})
+}
 
-function newContentCreator(valueList,key,nextOrder){
-  let tableBody = document.querySelector('#table-body')
-  let tableHeader = document.querySelector('#table-head')
-  let newContentObj = []
+// Event listener for sorting
+tableHeader.addEventListener("click", function (e) {
+  let header = e.target;
+  let clickedId = header.id
+  tableFn.tableSort(header, contentObj, function (newContentList, nextOrder) {
+    // Table header (re)loading
+    tableFn.tableHeadLoader(headerObj, nextOrder, function (headRow) {
+      tableHeader.appendChild(headRow);
 
-  for(let values of valueList){
-    for(let content of contentObj){
-      if(values == content[key]){
-        newContentObj.push(content)
+      // Hide icons based on sort order
+      let upArrow = document.querySelector(`.${clickedId}UpArrow`)
+      let downArrow = document.querySelector(`.${clickedId}DownArrow`);
+      if(nextOrder == 'asc'){
+        upArrow.style.visibility = 'hidden'
+        downArrow.style.visibility = "visible";
       }
-    }
-  }
-  removeChildNode(tableBody)
-  removeChildNode(tableHeader)
-  tableLoader(headerObj,newContentObj,nextOrder,key)
-}
+      else{
+        upArrow.style.visibility = "visible";
+        downArrow.style.visibility = "hidden";
+      }
+    });
 
-// Remove table content
-function removeChildNode(parent){
-  while(parent.firstChild){
-    parent.removeChild(parent.firstChild)
-  }
-}
+    // Table body (re)loading
+    tableFn.tableBodyLoader(tableBody,headerObj, newContentList);
+  });
+});
 
 
 
---
+++ resources/js/table.js
@@ -0,0 +1,208 @@
+
+/*------------------- Table Header loading fn -------------------*/
+
+let tableHeadLoader = function(headerList,order,callback){
+  if(headerList){  // Checks whether a table exists.
+    let headRow = document.createElement("tr");
+
+    for (let headValue of headerList){
+      let headElement = document.createElement("th")
+
+      // Check the table sorting direction and add class name accordingly.
+      if (order == "asc") {
+        headElement.className += "asc"
+      } else {
+        headElement.className += "des"
+      }
+  
+      // Adding text content and data attribute to 'th' element
+      headElement.textContent = headValue.tittle;
+      headElement.setAttribute("data-type", headValue.type);
+      headElement.setAttribute("data-sortable", headValue.sortable);
+      headElement.id = headValue.id;
+
+      // Adding up and down arrows
+      if(headValue.sortable == true){
+        
+        let upArrow = document.createElement('i')
+        upArrow.className = `${headValue.id}UpArrow up Arrow`;
+
+        let downArrow = document.createElement('i')
+        downArrow.className = `${headValue.id}DownArrow down Arrow`;
+
+        headElement.appendChild(upArrow)
+        headElement.appendChild(downArrow);
+      }
+
+      headRow.appendChild(headElement);
+    }
+
+    callback(headRow);
+
+  }
+}
+
+
+/*------------------- Table content loading fn -------------------*/
+
+let tableBodyLoader = function(tableBody,headerList,contentList){
+  if(contentList){  // Checks if any content present
+
+    for(let contentValue of contentList){
+      let rowElement = document.createElement("tr")
+
+      for (let headerValue of headerList){
+        let columnElement = document.createElement("td");
+        let key = headerValue.id;
+        let cellValue = contentValue[key];
+
+        if (headerValue.type == "link") {
+          let link = document.createElement("a");
+          link.href = cellValue;
+          link.textContent = "Link";
+          columnElement.appendChild(link);
+        } else if (headerValue.type == "button") {
+          if (contentValue.status == "open") {
+            let btn = document.createElement("button");
+            btn.textContent = "Apply Now";
+            btn.onclick = function (e) {
+              alert("Applies Successfully");
+            };
+            columnElement.appendChild(btn);
+          } else {
+            columnElement.textContent = " - ";
+          }
+        } else if (headerValue.type == "number" || headerValue.type == "date") {
+          columnElement.style.textAlign = "right";
+          columnElement.textContent = cellValue;
+          columnElement.className = key;
+        } else {
+          columnElement.textContent = cellValue;
+          columnElement.className = key;
+        }
+
+        rowElement.appendChild(columnElement);
+      }
+      tableBody.appendChild(rowElement);
+    }
+  }
+  else{
+    // Fallback fn here.......................................
+  }
+}
+
+
+/*------------------- Table sorting fn ------------------------*/
+
+let tableSort = function(header,contentList,callback){
+
+  // See if the data is sortable number
+  if (
+    header.dataset["sortable"] == "true" &&
+    header.dataset["type"] == "number"
+  ) {
+    let element = document.querySelectorAll(`.${header.id}`);
+    let valueList = [];
+
+    for (let each of element) {
+      valueList.push(parseInt(each.textContent));
+    }
+
+    if (header.className == "asc") {
+      var nextOrder = "des";
+      valueList.sort((a, b) => a - b);
+      var newContentList = newContentCreator(valueList,contentList, header.id, nextOrder);
+      callback(newContentList, nextOrder);
+      
+    } else {
+      var nextOrder = "asc";
+      valueList.reverse((a, b) => a - b);
+      var newContentList = newContentCreator(valueList,contentList, header.id, nextOrder);
+      callback(newContentList, nextOrder);
+    }
+  }
+  // See if the data is sortable strings
+  else if (
+    header.dataset["sortable"] == "true" &&
+    header.dataset["type"] == "string"
+  ) {
+    let element = document.querySelectorAll(`.${header.id}`);
+    let valueList = [];
+
+    for (let each of element) {
+      valueList.push(each.textContent);
+    }
+
+    if (header.className == "asc") {
+      var nextOrder = "des";
+      valueList.sort();
+      var newContentList = newContentCreator(valueList,contentList, header.id, nextOrder);
+      callback(newContentList, nextOrder);
+    } else {
+      var nextOrder = "asc";
+      valueList.reverse();
+      var newContentList = newContentCreator(valueList,contentList, header.id, nextOrder);
+      callback(newContentList, nextOrder);
+    }
+  }
+  // See if the data is sortable date (string type)
+  else if (
+    header.dataset["sortable"] == "true" &&
+    header.dataset["type"] == "date"
+  ) {
+    let element = document.querySelectorAll(`.${header.id}`);
+    let valueList = [];
+
+    for (let each of element) {
+      valueList.push(each.textContent);
+    }
+
+    if (header.className == "asc") {
+      var nextOrder = "des";
+      valueList.sort();
+      var newContentList = newContentCreator(valueList,contentList, header.id, nextOrder);
+      callback(newContentList, nextOrder);
+    } else {
+      let nextOrder = "asc";
+      valueList.reverse();
+      var newContentList = newContentCreator(valueList,contentList, header.id, nextOrder);
+      callback(newContentList, nextOrder);
+    }
+   
+  } else {
+    console.log("Not sortable");
+  }
+}
+
+
+
+// New table according to sort
+function newContentCreator(valueList, contentList, key, nextOrder) {
+  let tableBody = document.querySelector("tbody");
+  let tableHeader = document.querySelector("thead");
+  
+  let newContentObj = [];
+
+  for (let values of valueList) {
+    for (let content of contentList) {
+      if (values == content[key]) {
+        newContentObj.push(content);
+      }
+    }
+  }
+  removeChildNode(tableBody);
+  removeChildNode(tableHeader);
+  return newContentObj;
+}
+
+// Remove table content
+function removeChildNode(parent){
+  while(parent.firstChild){
+    parent.removeChild(parent.firstChild)
+  }
+}
+
+
+
+let tableFn = { tableHeadLoader, tableBodyLoader, tableSort };
+export default tableFn
